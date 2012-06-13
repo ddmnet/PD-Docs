963397e1b668cfbd5203c75b71d6232b3db3c227
Paul DeLeeuw <pauld@teamddm.com>
2012-06-13 10:14:10 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# About PD's Docs
+
+I wanted a really simple list of documents I'm currently working on in Markdown,
+and I wanted a way to have them parsed, and I wanted a way to share them with somebody.
+
+So I built PD's Docs.  It's a super-basic websitey thing that lists all the .md docs in
+a directory, and links them up to get rendered.
+
+Theres a couple config variables right up top that you can modify to customize.
+
+This is use-as-you-like; it's not super robust and it's probably extremely fragile.  It
+works for me, try it out.  Pull requests welcome.
+
+## Credits
+
+Of course, I just made the glue.  The heavy lifting is from two very fine projects:
+
+ * [Twitter Bootstrap](http://twitter.github.com/bootstrap/)
+ * [PHP Markdown Extra](http://michelf.com/projects/php-markdown/extra/)
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+$directory = dirname(__FILE__) . '/docs';
+
+$filenames = array();
+$iterator = new DirectoryIterator($directory);
+
+$uri = $_SERVER['REQUEST_URI'];
+$body = '';
+
+/**
+ * Config yousef' right here:
+ */
+$sitename = "Docs";
+$copyright = "&copy; Paul DeLeeuw, " . date('Y');
+
+if ($uri == '/') {
+	$title = $sitename;
+	$body .= "<h2>Index</h2>";
+	$body .= '<ul>';
+	foreach ($iterator as $fileinfo) {
+		if ($fileinfo->isFile() && preg_match('/\.md$/', $fileinfo->getFilename())) {
+			$body .= '<li><a href="/view/' . $fileinfo->getFilename() . '">' . $fileinfo->getFilename() . '</a></li>';
+		}
+	}
+	$body .= '</ul>';
+} else {
+	$uriComponents = explode('/', $uri);
+	
+	$action = $uriComponents[1];
+	$file = $uriComponents[2];
+	
+	$filename = dirname(__FILE__) . '/docs/' . $file;
+	
+	if (file_exists($filename)) {
+		include_once('lib/markdown.php');
+		$contents = file_get_contents($filename);
+		$body = Markdown($contents);
+		$title = $sitename . ": Viewing $file";
+	}
+	
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?=$title?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="/assets/css/bootstrap.css" />
+		<style type="text/css" media="screen">
+			
+			body {
+				padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+			}
+    
+		</style>
+		<link rel="stylesheet" href="/assets/css/bootstrap-responsive.css" />
+	</head>
+	<body>
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="brand" href="/"><?=$sitename?></a>
+					<!--<div class="nav-collapse">
+						<ul class="nav">
+						<?php
+						if (isset($file)):
+						?>
+							<li><a href="/">Home</a></li>
+						<?php
+						endif;
+						?>
+						</ul>
+					</div>
+					-->
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<?=$body?>
+			<hr>
+			<footer>
+				<p><?=$copyright?></p>
+			</footer>
+		</div>
+	</body>
+</html>
